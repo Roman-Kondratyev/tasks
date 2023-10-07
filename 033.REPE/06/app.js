1ec47cc2af41d88bd4c739a66b8756cb90992fad
Roman-Kondratyev <romankondratyev10@gmail.com>
2023-10-07 23:53:39 +0300

    JS

--- 033.REPE/06/app.js
+++ 033.REPE/06/app.js
@@ -10,5 +10,42 @@
 // этим массивом осуществляется только в repository. Массив находится в
 // приложении.
 
+class Server {
+    controller(obj) {
+        try {
+            const serv = this.service(obj);
+            return serv;
+        } catch (error) {
+            return error.message
+        }
 
+    }
+    service(obj){
+        const rep = this.repository(obj);
+        return rep;
+
+    }
+
+    repository(obj){
+        const arr = [
+            { "id": 1, "email": "yesenia@mail.ru", "pwd": "pwdffff" },
+            { "id": 2, "email": "hanna@mail.ru", "pwd": "pwdfevcrdv" },
+            { "id": 3, "email": "stanislau@mail.ru", "pwd": "pwdtest" },
+            { "id": 4, "email": "german@mail.ru", "pwd": "pwdqqq" },
+            { "id": 5, "email": "maria@mail.ru", "pwd": "pwdfcel" }
+        ]
+        const new_arr = arr.filter((el) => el.email == obj.email);
+        if (!new_arr.length) {
+            arr.push(obj);
+        }else {
+            throw new Error ('error')
+        }
+        return arr;
+    }
+
+}
+
+const server = new Server();
+const obj = { "id": 6, "email": "Test", "age": 1 };
+console.log(server.controller());
 
